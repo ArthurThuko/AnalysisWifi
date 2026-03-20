c6359030ee64c57b062ab274fd4d748a5faac2b5
ArthurThuko <arthurhenriqsfc@gmail.com>
2026-03-20 12:27:41 -0300

    Adicionando a tela Home do Aplicativo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import React, { JSX } from "react";
+import { useColorScheme } from "react-native";
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,53 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>App rodando 🚀</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/Logomarca-AnalysisWifi.png")}
+      />
+
+      <TouchableOpacity onPress={() => router.push('/redesEscaneadas')} style={styles.button}>
+        <Text style={styles.textButton}>Escanear Redes</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  logo: {
+    marginBottom: 50,
+    width: "100%",
+    height: "13%",
+  },
+  button: {
+    backgroundColor: "#1F7FB6",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    width: "100%",
+  },
+  textButton: {
+    fontSize: 24,
+    textAlign: "center",
+    color: "#fff",
+  }
+});
